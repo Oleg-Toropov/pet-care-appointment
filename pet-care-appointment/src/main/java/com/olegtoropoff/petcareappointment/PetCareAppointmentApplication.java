1a44b16fc4675073e3f11b7eaa99b7eb4459d1b4
Oleg-Toropov <toropovoleg1987@gmail.com>
2024-09-30 06:02:30 +0500

    Initial commit
    - Add basic project structure generated by Spring Initializr
    - Include dependencies: Spring Web, Spring Data JPA, Lombok, MySQL Driver
    - Add .gitignore to ignore unnecessary files

---
+++ pet-care-appointment/src/main/java/com/olegtoropoff/petcareappointment/PetCareAppointmentApplication.java
@@ -0,0 +1,13 @@
+package com.olegtoropoff.petcareappointment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetCareAppointmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetCareAppointmentApplication.class, args);
+	}
+
+}
---
+++ pet-care-appointment/src/test/java/com/olegtoropoff/petcareappointment/PetCareAppointmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.olegtoropoff.petcareappointment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetCareAppointmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
